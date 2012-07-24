47647c44903946ca78968afbe44c5e2f1a1ac04f
tERry <terrykingcha@gmail.com>
2012-07-24 17:36:52 +0800

    bug修改

--- normal.js
+++ normal.js
@@ -51,7 +51,7 @@ var norjs;
 		return function(id) {
 			var exports;
 
-			if (id.indexOf('@') < 0) id += '@';
+			if (id.indexOf(ID_SEP) < 0) id += ID_SEP;
 
 			for (var _id in depslist) {
 				if (_id.lastIndexOf(id) >=0) {
